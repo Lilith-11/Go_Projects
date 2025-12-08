15755da9552947b5dfae6059f1e9dae756d2f492
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-08 11:18:11 +0530

    student score updater

--- student_score_using_pointer.go
+++ student_score_using_pointer.go
@@ -1,1 +1,15 @@
-
+package main 
+import "fmt"
+type student struct{
+	name string
+	score int
+}
+func updatescore(m *int,add int){
+	*m =*m+add
+}
+func main(){
+    s:=student{name:"lilith",score:80}
+	fmt.Println("before score",s.score)
+	updatescore(&s.score,10)
+	fmt.Println("after score",s.score)
+}
